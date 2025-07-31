a60632a8bdad563412f9a6b93d5164eda9fe77a0
drink999water999 <mohamedronaldo512@gmail.com>
2025-07-31 13:21:14 +0300

    Delete components directory

--- components/ActivityFeed.tsx
+++
@@ -1,8 +0,0 @@
-
-import React from 'react';
-
-// This component is not currently used in the application.
-// Clearing its content to avoid confusion and unused code.
-export const ActivityFeed: React.FC = () => {
-  return null;
-};
